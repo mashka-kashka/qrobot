8f7488d133f2885f536c6807f77a9aef95c553c1
Maria Muravieva <maria.muravieva@mail.ru>
2025-02-09 17:05:31 +0300

    Исправлена начальная настройка камеры

--- application/camera.py
+++ application/camera.py
@@ -1,4 +1,4 @@
-from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot, QTimer
+from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot
 from PyQt6.QtGui import QImage
 from PyQt6.QtMultimedia import QCamera, QImageCapture, QMediaCaptureSession
 import numpy as np
@@ -12,23 +12,21 @@ class QRobotCamera(QObject):
     camera = None
     running = False
 
-    def __init__(self, camera_index=0):
+    def __init__(self):
         super().__init__()
+
+        self.format = 'RGB888'
+        self.size = (640, 480)
         with open('config.toml', 'r') as f:
             self.config = toml.load(f)
-        self.camera_index = camera_index
-
-    @pyqtSlot()
-    def start(self):
-        self.running = True
+            self.format = self.config["camera"]["format"]
+            self.size = (self.config["camera"]["width"], self.config["camera"]["height"])
         try:
             if platform.uname().node == "raspberrypi":
                 from picamera2 import Picamera2
                 self.picam2 = Picamera2()
                 self.picam2.configure(self.picam2.create_preview_configuration(
-                    main={"format": 'RGB888', "size": (self.config["camera"]["width"], self.config["camera"]["height"])}))
-                self.picam2.start() # запускаем камеру
-                self.get_frame()
+                    main={"format": self.format, "size": self.size}))
             else:
                 self.capture_session = QMediaCaptureSession()
                 self.camera = QCamera()
@@ -36,10 +34,17 @@ class QRobotCamera(QObject):
                 self.image_capture = QImageCapture(self.camera)
                 self.capture_session.setImageCapture(self.image_capture)
                 self.image_capture.imageCaptured.connect(self.on_image_captured)
-                self.camera.start()
-                self.get_frame()
         except Exception as e:
             print(f'Ошибка камеры: {str(e)}')
+
+    @pyqtSlot()
+    def start(self):
+        self.running = True
+        if self.picam2:
+            self.picam2.start()
+        if self.camera:
+            self.camera.start()
+        self.get_frame() # Получение первого кадра
 
     def stop(self):
         self.running = False
